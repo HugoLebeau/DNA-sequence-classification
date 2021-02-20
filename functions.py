7576816f69ab3b39e147f5d5e215d6e26d9ee64a
Hugo Lebeau <hug97@hotmail.fr>
2021-02-20 10:19:16 +0100

    typo

--- functions.py
+++ functions.py
@@ -8,4 +8,4 @@ def printStats(predicted, true):
     precision = np.sum(predicted & true)/np.sum(predicted) # TP / (TP + FP)
     recall = np.sum(predicted & true)/np.sum(true) # TP / (TP + FN)
     accuracy = (np.sum(predicted & true)+np.sum((1-predicted) & (1-true)))/len(true) # (TP + TN) / (P + N)
-    print("Precision\tRecall\tAccuracy\n{:.1%}\t\t{:.1%}\t{:.1%}\n".format(precision, recall, accuracy))
+    print("Precision\tRecall\tAccuracy\n{:.1%}\t\t{:.1%}\t{:.1%}".format(precision, recall, accuracy))
