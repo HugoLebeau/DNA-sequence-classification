7bac27341a32d272c972a9d5fc08bceb27582660
ilyeser <ilyes.er-rammach@ensta-paristech.fr>
2021-02-25 10:13:39 +0100

    print the steps

--- start.py
+++ start.py
@@ -9,12 +9,15 @@ from models import LogisticRegression, KernelRidgeRegression, KernelLogisticRegr
 
 np.random.seed(14159)
 
-method = "KernelSVM" # "LogReg" / "KernelRidge" / "KernelLogReg" / "KernelSVM"
+method = "KernelLogReg" # "LogReg" / "KernelRidge" / "KernelLogReg" / "KernelSVM"
 kernel = linear_kernel()
+
+print("choosen method :" , method)
 
 # %% LOAD DATA
 
 # Training data
+print("\nloading the data...")
 Xtr0 = pd.read_csv("data/Xtr0.csv", index_col="Id")
 Xtr1 = pd.read_csv("data/Xtr1.csv", index_col="Id")
 Xtr2 = pd.read_csv("data/Xtr2.csv", index_col="Id")
@@ -40,13 +43,15 @@ Xte0_mat100 = pd.read_csv("data/Xte0_mat100.csv", sep=" ", header=None)
 Xte1_mat100 = pd.read_csv("data/Xte1_mat100.csv", sep=" ", header=None)
 Xte2_mat100 = pd.read_csv("data/Xte2_mat100.csv", sep=" ", header=None)
 Xte_mat100 = pd.concat([Xte0_mat100, Xte1_mat100, Xte2_mat100], axis=0).reset_index(drop=True)
+print("done.\n")
 
 # %% TRAIN/EVAL SPLIT
-
+print("splitting...")
 prop_eval = 10/100  # proportion of the training set dedicated to evaluation
 id_eval = np.random.choice(Xtr.index, np.int(Xtr.shape[0]*prop_eval), replace=False)
 id_eval = np.isin(np.arange(Xtr.shape[0]), id_eval)
 id_train = ~id_eval
+print("done.\n")
 
 # %%
 
@@ -60,17 +65,18 @@ elif method == "KernelLogReg":
     model = KernelLogisticRegression(kernel, l2reg=1e-5)
 elif method == "KernelSVM":
     model = KernelSVM(kernel, l2reg=1e-5)
-
+print("training...")
 model.fit(Xtr_mat100[id_train].values, Ytr["Bound"][id_train].values)
+print("done.\n")
 predicted = model.predict(Xtr_mat100[id_eval])
-
+print("printing...")
 plt.hist(predicted, edgecolor='black', bins=20)
 plt.axvline(0.5, ls='--', color='black')
 plt.title("Histogram of predicted values")
 plt.show()
 
 predicted_labels = np.where(predicted > 0.5, 1, 0)
-print(method)
+
 printStats(predicted_labels, Ytr["Bound"][id_eval].values)
 
 model.fit(Xtr_mat100.values, Ytr["Bound"].values)
