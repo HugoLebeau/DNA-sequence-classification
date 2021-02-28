7c761f884099bdf0cd82fffb77d7b84513578b35
Hugo Lebeau <hug97@hotmail.fr>
2021-02-28 19:39:39 +0100

    different threshold + better plot

--- start.py
+++ start.py
@@ -10,14 +10,14 @@ from models import LogisticRegression, KernelRidgeRegression, KernelLogisticRegr
 np.random.seed(14159)
 
 method = "KernelSVM" # "LogReg" / "KernelRidge" / "KernelLogReg" / "KernelSVM"
-kernel = linear_kernel()
+kernel = RBF_kernel(sigma=1e-1)
+# kernel = linear_kernel()
 
-print("choosen method :" , method)
+print("Chosen method: {}.\n".format(method))
 
 # %% LOAD DATA
 
 # Training data
-print("\nloading the data...")
 Xtr0 = pd.read_csv("data/Xtr0.csv", index_col="Id")
 Xtr1 = pd.read_csv("data/Xtr1.csv", index_col="Id")
 Xtr2 = pd.read_csv("data/Xtr2.csv", index_col="Id")
@@ -43,44 +43,51 @@ Xte0_mat100 = pd.read_csv("data/Xte0_mat100.csv", sep=" ", header=None)
 Xte1_mat100 = pd.read_csv("data/Xte1_mat100.csv", sep=" ", header=None)
 Xte2_mat100 = pd.read_csv("data/Xte2_mat100.csv", sep=" ", header=None)
 Xte_mat100 = pd.concat([Xte0_mat100, Xte1_mat100, Xte2_mat100], axis=0).reset_index(drop=True)
-print("done.\n")
 
 # %% TRAIN/EVAL SPLIT
-print("splitting...")
+
 prop_eval = 10/100  # proportion of the training set dedicated to evaluation
 id_eval = np.random.choice(Xtr.index, np.int(Xtr.shape[0]*prop_eval), replace=False)
 id_eval = np.isin(np.arange(Xtr.shape[0]), id_eval)
 id_train = ~id_eval
-print("done.\n")
 
-# %%
+# %% PREDICTION
 
 Yte_predicted = pd.DataFrame(index=Xte.index)
 
 if method == "LogReg":
     model = LogisticRegression()
+    threshold = 0.5
 elif method == "KernelRidge":
     model = KernelRidgeRegression(kernel, l2reg=1e-5)
+    threshold = 0.5
 elif method == "KernelLogReg":
     model = KernelLogisticRegression(kernel, l2reg=1e-5)
+    threshold = 0.5
 elif method == "KernelSVM":
     model = KernelSVM(kernel, l2reg=1e-5)
-print("training...")
-model.fit(Xtr_mat100[id_train].values, Ytr["Bound"][id_train].values)
-print("done.\n")
-predicted = model.predict(Xtr_mat100[id_eval])
-print("printing...")
-plt.hist(predicted, edgecolor='black', bins=20)
-plt.axvline(0.5, ls='--', color='black')
-plt.title("Histogram of predicted values")
-plt.show()
+    threshold = 0.
 
-predicted_labels = np.where(predicted > 0.5, 1, 0)
+print("Training...")
+model.fit(Xtr_mat100[id_train].values, Ytr["Bound"][id_train].values)
+print("Done.\n")
+predicted = model.predict(Xtr_mat100[id_eval].values)
+predicted_labels = np.where(predicted > threshold, 1, 0)
+
+predicted0 = predicted[Ytr["Bound"][id_eval].values == 0]
+predicted1 = predicted[Ytr["Bound"][id_eval].values == 1]
+plt.hist([predicted0, predicted1], stacked=True, edgecolor='black', bins=20, label=[0, 1])
+plt.axvline(threshold, ls='--', color='black')
+plt.title("Histogram of predicted values")
+plt.legend(title="True label")
+plt.show()
 
 printStats(predicted_labels, Ytr["Bound"][id_eval].values)
 
+print("\nTraining...")
 model.fit(Xtr_mat100.values, Ytr["Bound"].values)
-Yte_predicted[method] = np.where(model.predict(Xte_mat100) > 0.5, 1, 0)
+print("Done.")
+Yte_predicted[method] = np.where(model.predict(Xte_mat100) > threshold, 1, 0)
 
 # %% SAVE PREDICTED VALUES
 
