62f1534958d6a56d4ddca0dc5dbd1a9f076ed1d0
Hugo Lebeau <hug97@hotmail.fr>
2021-03-15 12:05:46 +0100

    mismatch kernel

--- kernels.py
+++ kernels.py
@@ -1,6 +1,9 @@
 import numpy as np
+from tqdm import tqdm
 from scipy.sparse import csr_matrix
 from scipy.spatial.distance import cdist
+
+from trie import Trie
 
 class RBF_kernel(object):
     def __init__(self, sigma=1.):
@@ -39,13 +42,40 @@ class spectrum_kernel(object):
             Only words already seen in X1 are considered in X2.
             '''
             indptr, indices, data = [0], [], [] # these will be filled in order to build a CSR matrix
-            for seq in X:
+            for seq in tqdm(X):
                 for i in range(len(seq)-self.k+1):
                     if first or (seq[i:i+self.k] in vocabulary):
                         index = vocabulary.setdefault(seq[i:i+self.k], len(vocabulary))
                         indices.append(index)
                         data.append(1)
                 indptr.append(len(indices))
+            shape = None if first else (X2.shape[0], Phi[0].get_shape()[1])
+            Phi.append(csr_matrix((data, indices, indptr), dtype=int, shape=shape))
+            first = False
+        return Phi[0].dot(Phi[1].transpose()).toarray()
+
+class mismatch_kernel(object):
+    def __init__(self, k, m):
+        self.k = k
+        self.m = m
+        self.name = "({}, {})-mismatch kernel".format(k, m)
+    def __call__(self, X1, X2):
+        vocabulary, Phi = {}, []
+        first = True
+        trie = Trie()
+        for X in [X1, X2]:
+            indptr, indices, data = [0], [], [] # these will be filled in order to build a CSR matrix
+            for seq in tqdm(X):
+                for i in range(len(seq)-self.k+1):
+                    ls = [seq[i:i+self.k]] if first else trie.match(seq[i:i+self.k], self.m)
+                    for s in ls:
+                        index = vocabulary.setdefault(s, len(vocabulary))
+                        indices.append(index)
+                        data.append(1)
+                indptr.append(len(indices))
+            if first: # build Trie
+                for key in vocabulary.keys():
+                    trie.add_key(key)
             shape = None if first else (X2.shape[0], Phi[0].get_shape()[1])
             Phi.append(csr_matrix((data, indices, indptr), dtype=int, shape=shape))
             first = False
