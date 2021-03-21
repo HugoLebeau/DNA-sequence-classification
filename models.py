5ffab96968b03f5ac01dfcdf705f3e2c19d98637
Hugo Lebeau <hug97@hotmail.fr>
2021-03-21 22:24:37 +0100

    small changes

--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 import numpy as np
-from scipy import optimize
 from cvxopt import matrix, solvers
+from scipy import optimize
+from scipy.sparse import csr_matrix
 
 from functions import sigmoid, sigmoidp
 
@@ -255,8 +256,8 @@ class KernelSVM(object):
         n = X.shape[0]
         y = 2*y-1
         K = self.kernel(X, X)
-        P = matrix(2.*K, tc='d')
-        q = matrix(-2.*y, tc='d')
+        P = matrix(K, tc='d')
+        q = matrix(-y, tc='d')
         G = matrix(np.concatenate((-np.diag(y), np.diag(y)), axis=0), tc='d')
         h = matrix(np.block([np.zeros(n), np.ones(n)/(2.*n*self.l2reg)]), tc='d')
         sol = solvers.qp(P, q, G, h)
