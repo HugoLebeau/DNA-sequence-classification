5dadb11d396ad7be2af2077b2d95b844ca8928d0
Hugo Lebeau <hug97@hotmail.fr>
2021-03-21 12:10:10 +0100

    typo

--- trie.py
+++ trie.py
@@ -13,7 +13,7 @@ class Trie(object):
                 self.children[child] = Trie(depth=self.depth+1, value=child)
             self.children[child].add_key(key[1:])
     
-    def match_rec(self, key, upto, l=[], i=-1):
+    def match_rec(self, key, upto, l, i=-1):
         d_upto = 0 if i < 0 or key[i] == self.value[i] else 1
         if upto >= d_upto:
             if self.children == {}: #leaf
