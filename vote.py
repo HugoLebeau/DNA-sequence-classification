235f27fa5bf98869bf6a1e4c2647006229e37454
Hugo Lebeau <hug97@hotmail.fr>
2021-03-24 23:11:42 +0100

    Update vote.py

--- vote.py
+++ vote.py
@@ -2,10 +2,10 @@ import pandas as pd
 from datetime import datetime
 
 files = ["res/Yte_predicted_KernelSVM_2021-03-23_22-47-23.csv", # KernelSVM (9,1)-mismatch
+         "res/Yte_predicted_KernelSVM_2021-03-24_21-18-45.csv", # KernelSVM (11,2)-mismatch
          "res/Yte_predicted_KernelSVM_2021-03-24_07-17-22.csv", # KernelSVM (9,2)-mismatch
          "res/Yte_predicted_KernelLogReg_2021-03-23_22-09-40.csv", #KernelLogReg (9,0)-mismatch
-         "res/Yte_predicted_KernelSVM_2021-03-23_23-07-52.csv", #KernelSVM (10,1)-mismatch
-         "res/Yte_predicted_KernelSVM_2021-03-23_23-13-47.csv"] #KernelSVM (11,1)-mismatch
+         "res/Yte_predicted_KernelSVM_2021-03-23_23-07-52.csv"] #KernelSVM (10,1)-mismatch
 
 dfs = [pd.read_csv(file, index_col=0) for file in files]
 vote = (pd.concat(dfs, axis=1).sum(axis=1)/len(files)).round().astype(int)
